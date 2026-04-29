19086425e167b03774aca091f5d13c7b79ab38c7
David A. <avila_david_2@yahoo.com>
2026-04-29 09:39:05 -0500

    Began work on the main page

---
+++ frankies-sea-food/src/Pages/Homepage.js
@@ -0,0 +1,10 @@
+function Homepage(){
+    return(
+        <div>
+            <h1>Frankies Sea Food</h1>
+            <p>Welcome to Frankies Sea Food, best sea food in the South</p>
+        </div>
+    );
+}
+
+export default Homepage;
